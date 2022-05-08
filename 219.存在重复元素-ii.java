d2b871d3401d784c3802bfd434558a6ea69e0017
武子秋 <wuziqiu666@outlook.com>
2022-05-09 02:08:19 +0800

    219 226 232

---
+++ 219.存在重复元素-ii.java
@@ -0,0 +1,77 @@
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;;
+
+/*
+ * @lc app=leetcode.cn id=219 lang=java
+ *
+ * [219] 存在重复元素 II
+ *
+ * https://leetcode-cn.com/problems/contains-duplicate-ii/description/
+ *
+ * algorithms
+ * Easy (44.39%)
+ * Likes:    474
+ * Dislikes: 0
+ * Total Accepted:    179.1K
+ * Total Submissions: 403.4K
+ * Testcase Example:  '[1,2,3,1]\n3'
+ *
+ * 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且
+ * abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [1,2,3,1], k = 3
+ * 输出：true
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [1,0,1,1], k = 1
+ * 输出：true
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [1,2,3,1,2,3], k = 2
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * 0 <= k <= 10^5
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int i = 0;
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+        while (i < n) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            i++;
+        }
+        return false;
+    }
+}
+// @lc code=end
+
---
+++ 226.翻转二叉树.java
@@ -0,0 +1,86 @@
+import leetcodeUtil.*;
+
+/*
+ * @lc app=leetcode.cn id=226 lang=java
+ *
+ * [226] 翻转二叉树
+ *
+ * https://leetcode-cn.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (79.10%)
+ * Likes:    1278
+ * Dislikes: 0
+ * Total Accepted:    449.2K
+ * Total Submissions: 567.7K
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [4,2,7,1,3,6,9]
+ * 输出：[4,7,2,9,6,3,1]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [2,1,3]
+ * 输出：[2,3,1]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目范围在 [0, 100] 内
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        TreeNode tmp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(tmp);
+        return root;
+    }
+}
+// @lc code=end
+

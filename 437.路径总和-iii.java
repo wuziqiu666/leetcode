65cc1158d45834c917f635b0466590de0020ad1d
武子秋 <wuziqiu666@outlook.com>
2022-05-13 01:33:56 +0800

    401 437 355

---
+++ 437.路径总和-iii.java
@@ -0,0 +1,94 @@
+import leetcodeUtil.TreeNode;
+/*
+ * @lc app=leetcode.cn id=437 lang=java
+ *
+ * [437] 路径总和 III
+ *
+ * https://leetcode-cn.com/problems/path-sum-iii/description/
+ *
+ * algorithms
+ * Medium (57.04%)
+ * Likes:    1330
+ * Dislikes: 0
+ * Total Accepted:    164.1K
+ * Total Submissions: 288.8K
+ * Testcase Example:  '[10,5,-3,3,2,null,11,3,-2,null,1]\n8'
+ *
+ * 给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+ * 
+ * 路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+ * 输出：3
+ * 解释：和等于 8 的路径有 3 条，如图所示。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+ * 输出：3
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 二叉树的节点个数的范围是 [0,1000]
+ * -10^9  
+ * -1000  
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return 0;
+        }
+        int ret = 0;
+        ret += pathSum(root.left, targetSum);
+        ret += pathSum(root.right, targetSum);
+        ret += getSumt(root, targetSum, targetSum);
+        return ret;
+    }
+
+    public int getSumt(TreeNode root, int targetSum, int val) {
+        int sum = 0;
+        if (root == null) {
+            return 0;
+        }
+        if (root.val == val) {
+            sum += 1;
+        }
+        sum += getSumt(root.left, targetSum, val - root.val);
+        sum += getSumt(root.right, targetSum, val - root.val);
+        return sum;
+    }
+}
+// @lc code=end
+

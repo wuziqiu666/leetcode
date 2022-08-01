439529fc03cf52352503caae269b710eb1b99242
武子秋 <wuziqiu666@outlook.com>
2022-08-02 01:25:39 +0800

    31

---
+++ 31.下一个排列.java
@@ -0,0 +1,108 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+/*
+ * @lc app=leetcode.cn id=31 lang=java
+ *
+ * [31] 下一个排列
+ *
+ * https://leetcode.cn/problems/next-permutation/description/
+ *
+ * algorithms
+ * Medium (37.86%)
+ * Likes:    1844
+ * Dislikes: 0
+ * Total Accepted:    342.9K
+ * Total Submissions: 905.7K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 整数数组的一个 排列  就是将其所有成员以序列或线性顺序排列。
+ * 
+ * 
+ * 例如，arr = [1,2,3] ，以下这些都可以视作 arr 的排列：[1,2,3]、[1,3,2]、[3,1,2]、[2,3,1] 。
+ * 
+ * 
+ * 整数数组的 下一个排列 是指其整数的下一个字典序更大的排列。更正式地，如果数组的所有排列根据其字典顺序从小到大排列在一个容器中，那么数组的 下一个排列
+ * 就是在这个有序容器中排在它后面的那个排列。如果不存在下一个更大的排列，那么这个数组必须重排为字典序最小的排列（即，其元素按升序排列）。
+ * 
+ * 
+ * 例如，arr = [1,2,3] 的下一个排列是 [1,3,2] 。
+ * 类似地，arr = [2,3,1] 的下一个排列是 [3,1,2] 。
+ * 而 arr = [3,2,1] 的下一个排列是 [1,2,3] ，因为 [3,2,1] 不存在一个字典序更大的排列。
+ * 
+ * 
+ * 给你一个整数数组 nums ，找出 nums 的下一个排列。
+ * 
+ * 必须 原地 修改，只允许使用额外常数空间。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [1,2,3]
+ * 输出：[1,3,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [3,2,1]
+ * 输出：[1,2,3]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [1,1,5]
+ * 输出：[1,5,1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i+1, n - 1);
+
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    public void reverse(int[] nums, int i, int j) {
+        while (j > i) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+}
+// @lc code=end
+
+

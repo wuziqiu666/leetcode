03d4145c4e01754e21ea76db99c041044e66897f
武子秋 <wuziqiu666@outlook.com>
2022-08-06 02:56:45 +0800

    62

--- source/Test.java
+++ source/Test.java
@@ -3,5 +3,6 @@ import leetcodeUtil.ListNode;
 public class Test {
     public static void main(String[] args) {
         Solution solution = new Solution();
+        System.out.println(solution.uniquePaths(2, 2));
     }
 }
